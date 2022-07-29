de350ef8380c328b1db7003b2e83801f9d65752a
CakeCrusher <1sebastian1sosa1@gmail.com>
2022-07-29 19:18:51 -0400

    implemented data path config

--- tests/dev/test_mine.py
+++ tests/dev/test_mine.py
@@ -17,22 +17,33 @@ default_config = default_path / "config.ini"
 
 @pytest.fixture
 def mock_config_path(tmp_path):
+    print("mock_config_path tmp_path",tmp_path)
     tmp_config_path = tmp_path / "config.ini"
-    config.init_app(tmp_path)
+    data_path = tmp_path / "data"
+    config.init_app(tmp_path, data_path)
     # copy a file from path default_config and paste it to tmp_config_path
     tmp_config_path.write_text(default_config.read_text())
+    parsed_config = ConfigParser()
+    parsed_config.read(str(tmp_config_path))
+    parsed_config.set("general", "data_path", str(data_path))
+    # write into tmp_config_path
+    with open(str(tmp_config_path), "w") as config_file:
+        parsed_config.write(config_file)
+
     return tmp_config_path
 
 
 class TestMine:
     # Must have an initialized mimicbot config to the default parameters
-    def test_successful_mine(self, mock_config_path, tmp_path):
+    def test_successful_mine(self, tmp_path, mock_config_path):
+        print("test_successful_mine tmp_path",tmp_path)
         parsed_config = ConfigParser()
         parsed_config.read(str(mock_config_path))
         runner = CliRunner()
-        result = runner.invoke(cli.app, ["mine", "-ap", tmp_path])
+        app_path = mock_config_path.parent
+        result = runner.invoke(cli.app, ["mine", "--app-path", app_path])
 
-        data_path = tmp_path / "data" / \
+        data_path = app_path / "data" / \
             parsed_config.get("discord", "guild") / \
             parsed_config.get("general", "session")
         messages_path = data_path / "raw_messages.csv"
